f56b529fded3d5aa9d87be610ae90f2dbe57e9f6
George Vallencia <georgevallencia@gmail.com>
2025-08-26 10:40:16 +0300

    issues of deployment

---
+++ postcss.config.js
@@ -0,0 +1,31 @@
+
+// postcss.config.js
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
+
+
+// 
+
+// const config = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// };
+
+// export default config;
+
+
+// postcss.config.js
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+
